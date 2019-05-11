359695926b7c51da818b8719369227c7c30be59f
Danny Rangel <danielrangel.tech@gmail.com>
2019-05-11 04:22:51 -0700

    added inputs

--- src/DataForm.js
+++ src/DataForm.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import DataInput from './DataInput';
 
 const DataForm = () => {
     return (
-        <></>
+        <>
+            <form>
+                <DataInput label="Capacity"/>
+                <DataInput label="Average Seek Time"/>
+                <DataInput label="Spindle Speed"/>
+                <DataInput label="Bytes Per Sector"/>
+                <DataInput label="Sectors Per Cluster"/>
+                <DataInput label="Sectors Per Track"/>
+                <DataInput label="Records in File"/>
+                <DataInput label="Bytes in Record"/>
+                <button>Calculate</button>
+            </form>
+        </>
     );
 }
 
---
+++ src/DataInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DataInput = ({ label }) => {
+    return (
+        <>
+            <h4>{label}</h4>
+            <input></input>
+        </>
+    );
+}
+
+export default DataInput;
